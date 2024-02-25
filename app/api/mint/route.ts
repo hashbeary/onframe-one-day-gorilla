1517fc9f1b6eec07889cd0744bd8c7c81671230e
hashbeary <haslcastle@gmail.com>
2024-02-25 17:01:49 +0700

    fix: img src

--- app/api/mint/route.ts
+++ app/api/mint/route.ts
@@ -63,15 +63,12 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 		getFrameHtmlResponse({
 			buttons: [
 				{
-					label: "Mint",
+					label: "🦍 🍌 🔵",
 					action: "mint",
 					target: `eip155:7777777:0x7e35caf6abc257b5be0fbe7ef085807fd282c6ef:${tokenId}`,
 				},
 			],
-			image: {
-				src: `${NEXT_PUBLIC_URL}/nfts/${tokenId}.gif`,
-				aspectRatio: "1:1",
-			},
+			image: `${NEXT_PUBLIC_URL}/nfts/${tokenId}.gif`,
 		})
 	);
 }
