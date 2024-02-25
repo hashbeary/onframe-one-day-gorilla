d2cda0a0f7faddb016e46bf78db4776c0407a2c7
hashbeary <haslcastle@gmail.com>
2024-02-25 15:19:38 +0700

    move to zora

--- app/api/mint/route.ts
+++ app/api/mint/route.ts
@@ -1,118 +1,65 @@
-import { getFrameHtmlResponse } from "@coinbase/onchainkit";
+import {
+	FrameRequest,
+	getFrameHtmlResponse,
+	getFrameMessage,
+} from "@coinbase/onchainkit";
 import { NextRequest, NextResponse } from "next/server";
 import { NEXT_PUBLIC_URL } from "../../config";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-	return new NextResponse(
-		getFrameHtmlResponse({
-			buttons: [
-				{
-					label: "Mint is over!",
-				},
-				{
-					label: "Check collection",
-					action: "link",
-					target: `https://opensea.io/collection/one-day-gorillas`,
-				},
-			],
-			image: `${NEXT_PUBLIC_URL}/end.png`,
-		})
-	);
-
-	// const body: FrameRequest = await req.json();
-
-	// const { isValid, message } = await getFrameMessage(body, {
-	// 	neynarApiKey: "NEYNAR_ONCHAIN_KIT",
-	// });
-
-	// if (!isValid) {
-	// 	return new NextResponse(
-	// 		getFrameHtmlResponse({
-	// 			buttons: [
-	// 				{
-	// 					label: "Something went wrong. But you may retry!",
-	// 				},
-	// 			],
-	// 			image: `${NEXT_PUBLIC_URL}/error.jpg`,
-	// 			post_url: `${NEXT_PUBLIC_URL}/api/mint`,
-	// 		})
-	// 	);
-	// }
-
-	// const accountAddress: string | undefined =
-	// 	message.interactor.verified_accounts[0];
-
-	// const already_minted: { tx: string; tokenId: number } | null = await kv.get(
-	// 	"account:" + accountAddress
-	// );
-
-	// if (already_minted) {
-	// 	return new NextResponse(
-	// 		getFrameHtmlResponse({
-	// 			buttons: [
-	// 				{
-	// 					label: "You have already minted it!",
-	// 					action: "link",
-	// 					target: `https://basescan.org/tx/${already_minted.tx}`,
-	// 				},
-	// 				{
-	// 					label: "Collection",
-	// 					action: "link",
-	// 					target: `https://opensea.io/collection/one-day-gorillas`,
-	// 				},
-	// 			],
-	// 			image: {
-	// 				src: `${NEXT_PUBLIC_URL}/nfts/${already_minted.tokenId}.gif`,
-	// 				aspectRatio: "1:1",
-	// 			},
-	// 		})
-	// 	);
-	// }
-
-	// const tokenId = Math.round(Math.random() * 5 + 1);
-	// let tx: Hash;
-
-	// try {
-	// 	tx = await mint(accountAddress, tokenId);
-	// } catch (error) {
-	// 	await new Promise(resolve =>
-	// 		setTimeout(resolve, Math.round(Math.random() * 5000 + 2000))
-	// 	);
-	// 	return new NextResponse(
-	// 		getFrameHtmlResponse({
-	// 			buttons: [
-	// 				{
-	// 					label: "Something went wrong. But you may retry!",
-	// 				},
-	// 			],
-	// 			image: `${NEXT_PUBLIC_URL}/error.jpg`,
-	// 			post_url: `${NEXT_PUBLIC_URL}/api/mint`,
-	// 		})
-	// 	);
-	// }
-
-	// await kv.set("account:" + accountAddress, { tx, tokenId });
-
 	// return new NextResponse(
 	// 	getFrameHtmlResponse({
 	// 		buttons: [
 	// 			{
-	// 				label: "Successfully claimed!",
-	// 				action: "link",
-	// 				target: `https://basescan.org/tx/${tx}`,
+	// 				label: "Mint is over!",
 	// 			},
 	// 			{
-	// 				label: "Collection",
+	// 				label: "Check collection",
 	// 				action: "link",
 	// 				target: `https://opensea.io/collection/one-day-gorillas`,
 	// 			},
 	// 		],
-	// 		image: {
-	// 			src: `${NEXT_PUBLIC_URL}/nfts/${tokenId}.gif`,
-	// 			aspectRatio: "1:1",
-	// 		},
+	// 		image: `${NEXT_PUBLIC_URL}/end.png`,
 	// 	})
 	// );
+
+	const body: FrameRequest = await req.json();
+
+	const { isValid, message } = await getFrameMessage(body, {
+		neynarApiKey: "NEYNAR_ONCHAIN_KIT",
+	});
+
+	if (!isValid) {
+		return new NextResponse(
+			getFrameHtmlResponse({
+				buttons: [
+					{
+						label: "Something went wrong. But you may retry!",
+					},
+				],
+				image: `${NEXT_PUBLIC_URL}/error.jpg`,
+				post_url: `${NEXT_PUBLIC_URL}/api/mint`,
+			})
+		);
+	}
+
+	const tokenId = Math.round(Math.random() * 5 + 1);
+
+	return new NextResponse(
+		getFrameHtmlResponse({
+			buttons: [
+				{
+					label: "Mint",
+					action: "mint",
+					target: `eip155:7777777:0xe2af22fcd04e01ec88421cc8dba1e27e37f749af:${tokenId}`,
+				},
+			],
+			image: {
+				src: `${NEXT_PUBLIC_URL}/nfts/${tokenId}.gif`,
+				aspectRatio: "1:1",
+			},
+		})
+	);
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
